2e7c4777603323e37600fbf00da192d93a580b86
Victoria Dathan <vdathan18@gmail.com>
2021-09-21 18:58:34 -0400

    descriptive comment for code changes

---
+++ welcomeBack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int weatherTemps[] = {72, 85, 100, 90};
+
+    cout << weatherTemps [0];
+
+    cout << weatherTemps [1];
+
+    cout << weatherTemps [2];
+
+    cout << weatherTemps [3];
+}
